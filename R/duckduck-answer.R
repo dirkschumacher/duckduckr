84691cce44bc6f0180d8c573e5e97ff40ce34c15
Dirk Schumacher <mail@dirk-schumacher.net>
2017-04-05 18:40:02 +0200

    Add input checks and improve docs

--- R/duckduck-answer.R
+++ R/duckduck-answer.R
@@ -1,6 +1,6 @@
 #' DuckDuckGo's Instant Answer API
 #'
-#' Makes a synchronous call.
+#' Makes a synchronous API call.
 #'
 #' @param query the query string
 #' @param no_redirect TRUE to skip HTTP redirects (for !bang commands)
@@ -9,7 +9,7 @@
 #' @param app_name the appname used to identify your application. This is optional.
 #'
 #' If the JSON cannot be parsed then this function throws an error.
-#' If you want to be safe try to catch errors.
+#' If you want to be safe, try to catch those errors.
 #'
 #' @return Always returns a list with at least the slot `status`.
 #'         If the status is `OK`, then the result of the query is in the `result` slot.
@@ -18,9 +18,19 @@
 duckduck_answer <- function(query, no_redirect = FALSE,
                             no_html = FALSE, skip_disambig = FALSE,
                             app_name = "duckduckr") {
+
+  # lazy input checks
+  stopifnot(length(app_name) == 1)
+  stopifnot(length(no_redirect) == 1)
+  stopifnot(length(no_html) == 1)
+  stopifnot(length(skip_disambig) == 1)
+  stopifnot(is.character(app_name))
+  stopifnot(nchar(app_name) > 0)
   stopifnot(is.logical(no_redirect))
   stopifnot(is.logical(no_html))
   stopifnot(is.logical(skip_disambig))
+
+  # construct the api call
   client <- crul::HttpClient$new(url = "https://api.duckduckgo.com")
   query_params <- list(
     q = query,
@@ -31,6 +41,8 @@ duckduck_answer <- function(query, no_redirect = FALSE,
     t = app_name
   )
   result <- client$get("", query_params)
+
+  # format the result
   http_status <- result$status_http()$status_code
   if (http_status == "200") {
     list(
